6f57318ee661725f227094429c8910d3eca78e50
devoptimist <sbrown@chef.io>
2019-09-14 18:41:03 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sbrown@chef.io'
 license 'Apache-2.0'
 description 'Installs/Configures webapp'
 long_description 'Installs/Configures webapp'
-version '0.1.4'
+version '0.1.6'
 chef_version '>= 13.6.4'
 
 %w(windows).each do |os|
